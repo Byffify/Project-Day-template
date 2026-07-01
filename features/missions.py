7535654a9aefe0de1a13e6190599306b522d956e
Student <Student@Train11-5-PC115.control.ad.sit.kmutt.ac.th>
2026-07-01 16:29:43 +0700

    mission to send obj person

---
+++ features/missions.py
@@ -0,0 +1,12 @@
+man = {
+    "power":10,
+    "name":"saran",
+    "money":10000,
+    "role":"boss",
+    "age":20
+}
+
+def mission(man):
+    if man["power"] < 4:
+        return False
+    return True
